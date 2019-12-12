1fa2d54018064e26c4ed15e0e5be2a51126ba55c
kimbyungsoo <xfo5712@naver.com>
2019-12-12 23:04:31 +0900

    css

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-     header
+     <header>
+         <h1>TODO it!</h1>
+     </header>
   </div>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+h1 {
+    color: #2f2f2f;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
