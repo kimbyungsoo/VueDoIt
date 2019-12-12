ff91ea0a93ac6f37fa639beef6392df9d0a2482c
kimbyungsoo <xfo5712@naver.com>
2019-12-12 23:26:58 +0900

    save to local repo

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      input
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            newTodoItem : ""
+        }
+    },
+    methods: {
+        addTodo : function(){
+            console.log(this.newTodoItem);
+            //저장하는 로직
+            localStorage.setItem('key', this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
